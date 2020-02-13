ec65120523337cc0b90963a28a9e8cbc49e61648
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-13 22:19:16 +0700

    Create Rentals API

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,5 @@
 const { Movie, validateMovie } = require('../models/movie');
 const { Genre } = require('../models/genre');
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
