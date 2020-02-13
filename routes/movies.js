3aa53b46fcdf4708c4d563be447aa80128054e63
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-14 00:20:38 +0700

    Refactor post api of customers, genres, and movies and refactor genreId in movie models by using joi-objectid

--- routes/movies.js
+++ routes/movies.js
@@ -29,7 +29,7 @@ router.post('/', async (req, res) => {
         return res.status(400).send('Invalid genre.');
     }
 
-    let movie = new Movie({ 
+    const movie = new Movie({ 
         title: req.body.title,
         genre: {
             _id: genre._id,
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
         dailyRentalRate: req.body.dailyRentalRate
     });
 
-    movie = await movie.save();
+    await movie.save();
     res.send(movie);
 });
 
