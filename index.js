af7320926786d63f97c39283535a05cc2d74e984
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-19 19:38:13 +0700

    Handling rejected promises

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('express-async-errors');
+const error = require('./middleware/error');
 const { url, jwtPrivateKey } = require('./config/config');
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
@@ -25,6 +27,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3100;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
